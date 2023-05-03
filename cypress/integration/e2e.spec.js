a69f3191470d2c2da20d8af61e8c1e61c575b116
Gabriel Castilho <gabriel.castilho@outlook.com.br>
2023-05-02 23:18:23 -0300

    first commit

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -1,4 +1,9 @@
 /// <reference types="cypress" />
+import { faker } from '@faker-js/faker'
+const dataAddress = require('../fixtures/address.json')
+import enderecoPage from '../support/page_objects/endereco.page';
+//import { data } from 'cypress/types/jquery';
+
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     /*  Como cliente 
@@ -14,8 +19,53 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
-    });
 
+        let firstName = faker.name.firstName();
+        let lastName = faker.name.lastName();
+        let password = "!Teste@teste.com";
+        let email = faker.internet.email(firstName);
+        let quantity = Math.floor(Math.random() * (9 - 1) + 1);
+        cy.firstRegister(email, password, firstName, lastName)
+        //editar endereço de cobrança
+        enderecoPage.editBillingAddress(firstName,
+            lastName,
+            dataAddress[2].company,
+            dataAddress[2].country,
+            dataAddress[2].number,
+            dataAddress[2].city,
+            dataAddress[2].country,
+            dataAddress[2].state,
+            dataAddress[2].postalCode,
+            dataAddress[2].phoneNumber,
+            email
+        )
+        //editar endereço de entrega
+        enderecoPage.editShippingAddress(firstName,
+            lastName,
+            dataAddress[2].company,
+            dataAddress[2].country,
+            dataAddress[2].number,
+            dataAddress[2].city,
+            dataAddress[2].country,
+            dataAddress[2].state,
+            dataAddress[2].postalCode,
+        )
+        //adicionar produtos no carrinho
+      
+        cy.addProductOnCart(quantity);
+        //ir para pagamento
+        cy.checkout();
+
+    });
+    //it.only('deve adicionar produtos no carrinho', () => {
+        /* NÃO É POSSÍVEL DEIXAR TUDO AUTOMÁTICO, VISTO QUE EXISTEM PRODUTOS QUE SÓ POSSUI UMA COR, OU TAMANHO
+        let quantity = Math.floor(Math.random() * (9 - 1) + 1);
+        let arraySize = Math.floor(Math.random() * (5 - 1) + 1);
+        let arrayColor = Math.floor(Math.random() * (3 - 1) + 1);
+         cy.selectProducts(quantity);
+        cy.addProductOnCart(arraySize,arrayColor,quantity);
+        */
+       
+   // })
 
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,33 +1,72 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
-
 Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('#username').type(usuario)
-    cy.get('#password').type(senha, {log: false})
+    cy.get('#password').type(senha, { log: false })
     cy.get('.woocommerce-form > .button').click()
+
 });
 
+Cypress.Commands.add('firstRegister', (email, password, firstName, lastName) => {
+    cy.get('.icon-user-unfollow').click();
+    cy.get('#reg_email').type(email);
+    cy.get('#reg_password').type(password);
+    cy.get(':nth-child(4) > .button').click();
+    cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click();
+    cy.get('#account_first_name').type(firstName);
+    cy.get('#account_last_name').type(lastName);
+    cy.get('.woocommerce-Button').click();
+    cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.');
+
+})
+
+Cypress.Commands.add('addProductOnCart', (quantity,) => {
+    //adicionando primeiro item
+    cy.get('#primary-menu > .menu-item-629 > a').click();
+    cy.get('[class="product-block grid"]').first().click();
+    cy.get('[class="variable-item button-variable-item button-variable-item-M"]').click();
+    cy.get('.button-variable-item-Red').click();
+    cy.get('.input-text').clear().type(quantity);
+    cy.get('.single_add_to_cart_button').click();
+    cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantity);
+    
+   //adicionando segundo item
+   cy.get('#primary-menu > .menu-item-629 > a').click();
+    cy.get('[class="product-block grid"]').eq(8).click();
+    cy.get('[class="variable-item button-variable-item button-variable-item-XL"]').click();
+    cy.get('.button-variable-item-Blue').click();
+    cy.get('.input-text').clear().type(quantity);
+    cy.get('.single_add_to_cart_button').click();
+    cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantity *2);
+    //adicionado terceiro item
+    cy.get('#primary-menu > .menu-item-629 > a').click();
+    cy.get('[class="product-block grid"]').first().click();
+    cy.get('[class="variable-item button-variable-item button-variable-item-S"]').click();
+    cy.get('.button-variable-item-Green').click();
+    cy.get('.input-text').clear().type(quantity);
+    cy.get('.single_add_to_cart_button').click();
+    cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantity *3);
+    //adicionando quarto item
+    cy.get('#primary-menu > .menu-item-629 > a').click();
+    cy.get('[class="product-block grid"]').eq(8).click();
+    cy.get('[class="variable-item button-variable-item button-variable-item-M"]').click();
+    cy.get('.button-variable-item-Blue').click();
+    cy.get('.input-text').clear().type(quantity);
+    cy.get('.single_add_to_cart_button').click();
+    cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantity*4)
+
+    }) 
+
+Cypress.Commands.add('checkout',()=>{
+    cy.get('.woocommerce-message > .button').click();
+    cy.get('.checkout-button').click()
+    cy.get('#payment_method_cod').click();
+    //cy.get('[class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox"]').click();
+    cy.get('.woocommerce-terms-and-conditions-checkbox-text').click({ force:true });
+    cy.get('#place_order').click({force:true});
+    cy.get('.woocommerce-notice').should('contain','Obrigado. Seu pedido foi recebido.');
+})
+
+
+ //cy.get('[data-attribute_values='+ arraySize +']').click();
+   //cy.get('[data-attribute_values='+ arrayColor +']').click();
+    //cy.get('[class="variable-item button-variable-item button-variable-item-'+color+']"').click();
+    //cy.get('[class="variable-item button-variable-item button-variable-item-Red"]').click();
